649cc1c6bf2d0d74b0eba047abefda047952d1d1
HUAQING\yangg <1123435072@.com>
2017-07-08 19:42:07 +0800

    提交,保存一下代码

--- app/src/main/java/cn/zzu/googleplay/MainActivity.java
+++ app/src/main/java/cn/zzu/googleplay/MainActivity.java
@@ -1,5 +1,10 @@
 package cn.zzu.googleplay;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -8,6 +13,11 @@ import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 
+import com.astuetz.PagerSlidingTabStrip;
+
+import cn.zzu.googleplay.factory.FragmentFactory;
+import cn.zzu.googleplay.util.UIUtils;
+
 /**
  * 主界面
  */
@@ -15,51 +25,98 @@ public class MainActivity extends AppCompatActivity {
 
     private DrawerLayout mDrawLayout;
     private ActionBarDrawerToggle toggle;
+    private ViewPager vp;
+    private PagerSlidingTabStrip mTabs;
+    private String mMainTitles;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-        //ActionBar supportActionBar = getSupportActionBar();
-
         initActionBar();
         initView();
-
         intiactionBardrawToggle();
+
+        initData();
+    }
+
+    private void initData() {
+        //为viewpager设置适配器
+        mMainTitles = UIUtils.getString(R.array.main_titles);
+        vp.setAdapter(new MainPagerAdapter(getSupportFragmentManager()));
+        mTabs.setViewPager(vp);
 
     }
 
+    /**
+     * pagerAdapter view
+     *
+     */
+    class MainPagerAdapter extends FragmentPagerAdapter{
+
+
+
+        public MainFragmentPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        public  class  MainFragmentPagerAdapter e()
+
+        //返回指定position页面的Fragment
+        @Override
+        public Fragment getItem(int position) {
+            Fragment fragment = FragmentFactory.createFragment(position);
+            return null;
+        }
+
+        /**
+         * 决定viewpaget也是的综合
+         * @return
+         */
+        @Override
+        public int getCount() {
+            if (mMainTitles!=null){
+                return mMainTitles.length();
+            }
+            return 0;
+        }
+
+        /**
+         * 必须复写这个方法,得到title
+         * 是什么
+         * @param position
+         * @return
+         */
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mMainTitles[position];
+        }
+    }
+
     private void intiactionBardrawToggle() {
-
         toggle = new ActionBarDrawerToggle(this,mDrawLayout, R.string.open,R.string.close);
-
         //同步状态方法-->途欢默认回退部分的ui效果
         toggle.syncState();
-
         //监听 设置 drawlayout的监听//拖动的时候上面也 会跟着变化
         mDrawLayout.setDrawerListener(toggle);
     }
 
     private void initView() {
-
         mDrawLayout = (DrawerLayout) findViewById(R.id.main_drawer_layout);
+        vp = (ViewPager) findViewById(R.id.main_vp);
+        mTabs = (PagerSlidingTabStrip) findViewById(R.id.main_tabs);
+
 
 
     }
 
     private void initActionBar() {
-
-
-
         ActionBar supportActionBar = getSupportActionBar();//v4包中的action
         /**
          * 老师的方法
          */
         //supportActionBar.setCustomView();
-
-
 
         supportActionBar.setTitle("主标题");
         supportActionBar.setSubtitle("副标题");
