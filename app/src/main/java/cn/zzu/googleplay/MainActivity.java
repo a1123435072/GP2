65439647e428a3d29127868607abd93cc7031255
HUAQING\yangg <1123435072@.com>
2017-07-08 11:49:48 +0800

    第三次提交我的代码

--- app/src/main/java/cn/zzu/googleplay/MainActivity.java
+++ app/src/main/java/cn/zzu/googleplay/MainActivity.java
@@ -3,11 +3,16 @@ package cn.zzu.googleplay;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ * 主界面
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        
     }
 }
